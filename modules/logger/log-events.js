c07e5ebfb64e993e8a978ef350434b9341f5fdfc
Cristhiam Quiñonez Especialista en Marketing Digital | Estrategias de Crecimiento y SEO <chrisquionez354@gmail.com>
2025-07-02 00:40:36 -0600

    Actualizar log-events.js

--- modules/logger/log-events.js
+++ modules/logger/log-events.js
@@ -1,1 +1,10 @@
- 
+// modules/logger/log-events.js
+
+module.exports = (client) => {
+  client.on('messageDelete', (message) => {
+    const logChannel = message.guild.channels.cache.find(c => c.name === 'log-bot');
+    if (logChannel) {
+      logChannel.send(`🗑️ Mensaje borrado por ${message.author.tag}: "${message.content}"`);
+    }
+  });
+}; 
